40a409eb883a7b8746973a1b0b27ea5dedad61b1
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-05-14 21:11:05 -0300

    Add: Operadores

---
+++ Operadores/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string titulo, descricao;
+
+        titulo = Console.ReadLine();
+        descricao = Console.ReadLine();
+
+        if (descricao.Length > 50) {
+            Console.WriteLine("Descricao ultrapassa limite de caracteres.");
+        } else {
+            string dataLimite = Console.ReadLine();
+            Console.WriteLine($"{descricao} ate {dataLimite}");
+        }
+    }
+}
