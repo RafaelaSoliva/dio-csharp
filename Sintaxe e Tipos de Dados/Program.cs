7cd18bf0b2e9ab87e01daeec4191d17a9da0c97e
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-05-14 21:01:21 -0300

    Add: Sintaxe e Tipos de Dados

---
+++ Sintaxe e Tipos de Dados/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string email, nome, senha;
+
+        email = Console.ReadLine();
+        nome = Console.ReadLine();
+        senha = Console.ReadLine();
+        
+        Console.WriteLine($"{nome}, verifique o email: {email} para ativar.");
+    }
+}
