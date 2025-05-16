22638fda575ec099d3f6acbcfabdcccd3b442e17
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-05-16 16:22:50 -0300

    Add: Orientação a Objetos

---
+++ Orientação a Objetos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+    public Pessoa (string nome, int idade) {
+        Nome = nome;
+        Idade = idade;
+    }
+
+    public void Exibir() {
+        Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        string nome = Console.ReadLine();
+        int idade = int.Parse(Console.ReadLine());
+
+        Pessoa pessoa = new Pessoa(nome, idade);
+
+        pessoa.Exibir();
+    }
+}
