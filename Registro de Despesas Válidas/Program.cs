ba40b20557f0884c742b21fbf19c6d62280a564e
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-05-17 16:30:59 -0300

    Add: Registro de Despesas Válidas

---
+++ Registro de Despesas Válidas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int quantidadeDados = int.Parse(Console.ReadLine());
+
+        List<int> dados = new List<int>();
+        int entrada;
+
+        for (int i = 0; i < quantidadeDados; i++) {
+            entrada = int.Parse(Console.ReadLine());
+
+            if (entrada > 0) {
+                dados.Add(entrada);
+            }
+        }
+
+        if (dados.Count == 0) {
+            Console.WriteLine("Sem despesas validas");
+        } else {
+            foreach (int dado in dados) {
+                Console.WriteLine(dado);
+            }
+        }
+    }
+}
