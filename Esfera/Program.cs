14c298ae430aad528945fbff35fe0d128dc71f10
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-05-17 15:45:27 -0300

    Add: Esfera

---
+++ Esfera/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Esfera
+{
+    static void Main(string[] args)
+    {
+        double raio = double.Parse(Console.ReadLine());
+        double pi = 3.14159;
+        double volume = ((4.0 / 3.0) * pi * Math.Pow(raio, 3));
+
+        Console.WriteLine($"VOLUME = {Math.Round(volume, 3)}");
+    }
+}
