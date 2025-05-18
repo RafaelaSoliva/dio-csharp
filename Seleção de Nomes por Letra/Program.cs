a5702daaa2966539cd654e07ff80eed4594e6ee7
RafaelaSoliva <rafaelasoliva@outlook.com>
2025-05-18 15:23:56 -0300

    Add: Seleção de Nomes por Letra

---
+++ Seleção de Nomes por Letra/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string entradaNomes = Console.ReadLine();
+        char letraInicial = Convert.ToChar(Console.ReadLine().ToUpper());
+
+        string[] nomes = entradaNomes.Split(",");
+        List<string> nomesEncontrados = new List<string>();
+
+        for (int i = 0; i < nomes.Length; i++) {
+            if (nomes[i].ToUpper().TrimStart().StartsWith(letraInicial)) {
+                nomesEncontrados.Add(nomes[i].TrimStart());
+            }
+        }
+
+        if (nomesEncontrados.Count == 0) {
+            Console.WriteLine("Nenhum nome encontrado");
+        } else {
+            foreach (string nome in nomesEncontrados) {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
